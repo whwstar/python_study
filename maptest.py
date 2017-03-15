545f40e6ac34609ed484948b2b7a7994065ebcc1
wanghongwei <whwstar.whwstar@gmail.com>
2017-03-15 17:17:29 +0800

    utf-8 insert file

--- maptest.py
+++ maptest.py
@@ -1,3 +1,18 @@
+#!/bin/en python
+
 #!/bin/env python
 
-host_map={"all_host":{'hosts':['172,27.4.9','172.27.4.10']},"vars":{'user':root,'passwd':'123456'}}
+"""an example for Unicode insert file"""
+CODEC = 'utf-8'
+FILE = '/home/whwstar/unicode.txt'
+hello_out = u"Hello world\n"
+bytes_out = hello_out.encode(CODEC)
+f = open(FILE,"w")
+f.write(bytes_out)
+f.close()
+
+f = open(FILE,'r')
+bytes_in=f.read()
+aa = bytes_in.decode(CODEC)
+print aa
+
